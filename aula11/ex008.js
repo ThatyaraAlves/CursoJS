b9f2a854dcf02dae524b0fc7ef7bba15c96090da
ThatyaraAlves <87332375+ThatyaraAlves@users.noreply.github.com>
2022-02-10 17:27:20 -0300

    dom

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var velocidade = 50.0;
+console.log(`O seu carro está a ${velocidade}Km/h`)
+if (velocidade > 60) { //condição simples
+    console.log(`Você ultrapassou o limite permitido. MULTADO!`)
+}
+console.log('Dirija sempre com cuidado!')
